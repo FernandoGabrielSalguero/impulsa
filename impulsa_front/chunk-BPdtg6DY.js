d1ab17311080d61e4054e10b6314b968fb656fd0
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2026-07-06 15:50:09 -0300

    main

---
+++ impulsa_front/chunk-BPdtg6DY.js
@@ -0,0 +1,1 @@
+import {T,B as qe,bM as Dg,by as M}from'./main-WLQCQJMI.js';function w(t){t||(t=T(qe));let i=new M(e=>{if(t.destroyed){e.next();return}return t.onDestroy(e.next.bind(e))});return e=>e.pipe(Dg(i))}export{w};
